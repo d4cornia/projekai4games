eebb926f50b1540bc705f94bca791d49057ee244
d4cornia <56968953+d4cornia@users.noreply.github.com>
2021-05-01 15:21:48 +0700

    init

---
+++ Projek AI/Assets/Script/fieldOfView.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class fieldOfView : MonoBehaviour
+{
+    [SerializeField]
+    public LayerMask layerMask;
+    // layer mask digunakan untuk supaya raycast ngecek collide di layer apa
+    // yang ada di layermask yang akan dicek, yang tidak ada maka akan diignore
+
+    public Mesh mesh;
+    public Vector3 origin;
+    public float curAngle;
+    public float fov;
+    public float viewDistance;
+    public int rayCtr;
+    public GameObject obj;
+    public RaycastHit2D rayCast; // rayCast yang neghit object
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        this.origin = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        // banyak titik dalam mesh, makin banyak makin smooth
+
+        // current angle player 
+        float curAngle = this.curAngle;
+
+        // angle increase
+        float angleIncrease = fov / rayCtr;
+
+
+        Vector3[] vertices = new Vector3[rayCtr + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] tri = new int[rayCtr * 3];
+
+        vertices[0] = this.origin;
+
+        int vertexIndex = 1;
+        int triIndex = 0;
+        obj = null;
+        for (int i = 0; i <= rayCtr; i++)
+        {
+            Vector3 vertex;
+            // seperti collider tetapi raycast
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(this.origin, UtilsClass.GetVectorFromAngle(curAngle), viewDistance, layerMask); 
+            if (raycastHit2D.collider == null)
+            {
+                // no hit
+                vertex = this.origin + UtilsClass.GetVectorFromAngle(curAngle) * viewDistance;
+            }
+            else
+            {
+                // hit object
+                rayCast = raycastHit2D;
+                obj = raycastHit2D.collider.gameObject;
+                vertex = raycastHit2D.point;
+                // jadi jika kena object maka titik render hanya di point object kena raycast 2d itu
+            }
+
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0) // karena vertex origin tidak punya prev vertex
+            {
+                tri[triIndex + 0] = 0; // origin
+                tri[triIndex + 1] = vertexIndex - 1; // prev vertex
+                tri[triIndex + 2] = vertexIndex; // cur vertex
+                
+                triIndex += 3;
+            }
+
+            vertexIndex++;
+            curAngle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = tri;
+    }
+
+    public void setOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void setAimDirection(Vector3 aimDir)
+    {
+        this.curAngle = UtilsClass.GetAngleFromVectorFloat(aimDir) + fov / 2f;
+    }
+
+
+}
---
+++ Projek AI/Assets/Script/playerController.cs
@@ -0,0 +1,97 @@
+﻿using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class playerController : MonoBehaviour
+{ 
+
+    public Rigidbody2D rb;
+    public GameObject playerObj;
+    public float maxSpeed;
+    public Animator animator;
+    private Vector2 force;
+    private int look;
+
+
+    public Light2D light;
+
+   [SerializeField]
+    public fieldOfView fov;
+
+    void Awake()
+    {
+        if (rb == null)
+        {
+            playerObj = GameObject.Find("Player");
+            rb = playerObj.GetComponent<Rigidbody2D>();
+            animator = playerObj.GetComponent<Animator>();
+            fov = Instantiate(GameObject.Find("FieldOfView").GetComponent<fieldOfView>());
+            fov.name = "playerFOV";
+            light = playerObj.GetComponent<Light2D>();
+        }
+        look = 4;
+    }
+
+    private void Update()
+    {
+        processInput();
+    }
+
+    void FixedUpdate()
+    {
+        move();
+        walkAnim();
+        updateOrientationPlayer();
+        spriteOrientation();
+    }
+
+    private float moveX, moveY;
+
+    void processInput()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+
+        float xForce = moveX * maxSpeed;
+        float yForce = moveY * maxSpeed;
+
+        force = new Vector2(xForce, yForce);
+    }
+
+    void move()
+    {
+        if (moveX == 0 && moveY == 0)
+        {
+            rb.drag = 1;
+        }
+        rb.AddForce(force);
+    }
+
+    public void updateOrientationPlayer()
+    {
+        Vector3 targetPosition = UtilsClass.GetWorldPositionFromUI();
+        fov.setAimDirection((targetPosition - transform.position).normalized);
+        fov.setOrigin(transform.position);
+        light.transform.rotation = Quaternion.Euler(0, 0, fov.curAngle - fov.fov * 2f);
+    }
+
+    void spriteOrientation()
+    {
+        if (fov.curAngle > 60 && fov.curAngle < 170) look = 1;
+        else if (fov.curAngle > 170 && fov.curAngle < 250) look = 3;
+        else if (fov.curAngle > 250 && fov.curAngle < 330) look = 4;
+        else if (fov.curAngle < 60 || fov.curAngle > 330) look = 2;
+
+        animator.SetInteger("Look", look);
+        look = 0;
+    }
+
+    void walkAnim()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x + rb.velocity.y));
+        if (Mathf.Abs(rb.velocity.x + rb.velocity.y) > 2) animator.speed = 1.3f;
+        else animator.speed = 0.6f;
+    }
+}
