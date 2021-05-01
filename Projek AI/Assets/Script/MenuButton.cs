9639efddfba39d9d99b081d0c2d2bb94890b5df7
d4cornia <56968953+d4cornia@users.noreply.github.com>
2021-05-01 20:22:24 +0700

    menu and animation

---
+++ Projek AI/Assets/Script/MenuButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+	[SerializeField] AudioClip soundClick;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(menuButtonController.index == thisIndex)
+		{
+			animator.SetBool ("selected", true);
+			if(Input.GetAxis ("Submit") == 1){
+				animator.SetBool ("pressed", true);
+				animatorFunctions.PlaySound(soundClick);
+			}else if (animator.GetBool ("pressed")){
+				animator.SetBool ("pressed", false);
+				animatorFunctions.disableOnce = true;
+			}
+		}else{
+			animator.SetBool ("selected", false);
+		}
+    }
+}
