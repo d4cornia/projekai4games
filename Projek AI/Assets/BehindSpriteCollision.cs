99f4a8346b02b5fb5aa06f2c42cee355f76fd590
Yosua77 <yosuayuwono@gmail.com>
2021-06-25 04:14:57 +0700

    Basement

--- Projek AI/Assets/BehindSpriteCollision.cs
+++ Projek AI/Assets/BehindSpriteCollision.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class BehindSpriteCollision : MonoBehaviour
 {
@@ -8,8 +9,9 @@ public class BehindSpriteCollision : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-            sprite.sortingLayerName = "Layer 2";
+            SpriteRenderer sprite = collision.gameObject.GetComponent<SpriteRenderer>();
+            sprite.sortingOrder = 2;
+            findChild(gameObject, "Shadow").SetActive(true);
             Debug.Log("Enter :" + sprite.sortingLayerName);
         }
     }
@@ -17,9 +19,23 @@ public class BehindSpriteCollision : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-            sprite.sortingLayerName = "Layer 1";
+            SpriteRenderer sprite = collision.gameObject.GetComponent<SpriteRenderer>();
+            sprite.sortingOrder = 4;
+            findChild(gameObject, "Shadow").SetActive(false);
             Debug.Log("Exit :" + sprite.sortingLayerName);
         }
+    }
+
+    private GameObject findChild(GameObject parent, string name)
+    {
+        Transform[] trs = parent.GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in trs)
+        {
+            if (t.name == name)
+            {
+                return t.gameObject;
+            }
+        }
+        return null;
     }
 }
