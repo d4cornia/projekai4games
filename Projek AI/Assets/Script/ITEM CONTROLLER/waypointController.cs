370331c49af6aebd69c38f01e14bed5f9667c7bf
d4cornia <56968953+d4cornia@users.noreply.github.com>
2021-06-26 07:58:43 +0700

    item spawn

--- Projek AI/Assets/Script/ITEM CONTROLLER/waypointController.cs
+++ Projek AI/Assets/Script/ITEM CONTROLLER/waypointController.cs
@@ -8,19 +8,19 @@ public class waypointController : MonoBehaviour
 
     private void Awake()
     {
-        float r = Random.Range(0, 1);
-        if(r * 100 < 15)
+        int r = Random.Range(0, 100);
+        if(r < 15)
         {
-            int idx = Random.Range(0, 2);
+            int idx = Random.Range(0, 3);
             PFitems[idx + 7].transform.position = this.gameObject.transform.position;
             PFitems[idx + 7].layer = LayerMask.NameToLayer("Collectibles");
             PFitems[idx + 7].GetComponent<SpriteRenderer>().sortingLayerName = "Layer 2";
             PFitems[idx + 7].GetComponent<SpriteRenderer>().sortingOrder = 0;
-            Instantiate(PFitems[idx + 6]);
+            Instantiate(PFitems[idx + 7]);
         }
         else
         {
-            int idx = Random.Range(0, 7);
+            int idx = Random.Range(0, 8);
             PFitems[idx].transform.position = this.gameObject.transform.position;
             PFitems[idx].layer = LayerMask.NameToLayer("Collectibles");
             PFitems[idx].GetComponent<SpriteRenderer>().sortingLayerName = "Layer 2";
